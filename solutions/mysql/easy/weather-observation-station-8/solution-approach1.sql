afe4acf6545a4280451de9fcac49176f49981950
maisha-mehzabin <maishamehzabin0304@gmail.com>
2026-08-09 15:17:04 +0600

    ✅ Weather Observation Station 8 [mysql] — approach1

--- solutions/mysql/easy/weather-observation-station-8/solution-approach1.sql
+++ solutions/mysql/easy/weather-observation-station-8/solution-approach1.sql
@@ -6,7 +6,7 @@
 -- Platform    HackerRank
 -- Language    mysql
 -- Status      Accepted
--- Submitted   2026-08-09, 03:16 p.m.
+-- Submitted   2026-08-09, 03:17 p.m.
 -- ──────────────────────────────────────────────────
 
 /*
