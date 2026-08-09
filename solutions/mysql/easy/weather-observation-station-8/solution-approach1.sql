6b182a0bd7ff1e7cc4259c599b6624486c9035b2
maisha-mehzabin <maishamehzabin0304@gmail.com>
2026-08-09 15:16:50 +0600

    ✅ Weather Observation Station 8 [mysql] — approach1

---
+++ solutions/mysql/easy/weather-observation-station-8/solution-approach1.sql
@@ -0,0 +1,16 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-8/problem?isFullScreen=true
+-- Problem     Weather Observation Station 8
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-09, 03:16 p.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/SELECT DISTINCT CITY
+FROM STATION 
+WHERE CITY REGEXP '^[AEIOU].*[AEIOU]$'
