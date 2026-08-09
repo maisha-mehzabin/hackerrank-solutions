a4f5a3f05b4ce8ce9cbeb900a43e77fc6c7fdfdc
maisha-mehzabin <maishamehzabin0304@gmail.com>
2026-08-09 15:16:26 +0600

    ✅ Weather Observation Station 6 [mysql] — approach1

---
+++ solutions/mysql/easy/weather-observation-station-6/solution-approach1.sql
@@ -0,0 +1,16 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-6/problem?isFullScreen=true
+-- Problem     Weather Observation Station 6
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-09, 03:16 p.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%'
