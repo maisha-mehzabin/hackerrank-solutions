9e268f78b1602bd0351ae0a0676449f73eb3805c
maisha-mehzabin <maishamehzabin0304@gmail.com>
2026-08-09 19:23:28 +0600

    ✅ The Report [mysql] — approach1

---
+++ solutions/mysql/medium/the-report/solution-approach1.sql
@@ -0,0 +1,26 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/the-report/problem?isFullScreen=true
+-- Problem     The Report
+-- Difficulty  Medium
+-- Subdomain   Basic Join
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-09, 07:23 p.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/ (SELECT s.name, g.grade, s.marks
+FROM students s
+JOIN grades g ON s.marks BETWEEN g.min_mark AND g.max_mark
+WHERE g.grade >= 8)
+
+UNION
+
+(SELECT NULL, g.grade, s.marks
+FROM students s 
+JOIN grades g ON s.marks BETWEEN g.min_mark AND g.max_mark
+WHERE g.grade < 8)
+
+ORDER BY grade DESC, name ASC, marks ASC
