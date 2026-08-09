b50b0f7acb573a0a9d58125babacd80cc73d0f5c
maisha-mehzabin <maishamehzabin0304@gmail.com>
2026-08-09 15:15:27 +0600

    ✅ Weather Observation Station 1 [mysql] — approach1

---
+++ solutions/mysql/easy/weather-observation-station-1/solution-approach1.sql
@@ -0,0 +1,15 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
+-- Problem     Weather Observation Station 1
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-09, 03:15 p.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/SELECT CITY , STATE  
+FROM STATION 
