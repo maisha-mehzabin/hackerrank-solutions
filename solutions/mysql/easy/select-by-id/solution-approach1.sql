919c1445809bd2e4bedf6a670298d991d35a2e72
maisha-mehzabin <maishamehzabin0304@gmail.com>
2026-08-09 20:20:43 +0600

    ✅ Select By ID [mysql] — approach1

---
+++ solutions/mysql/easy/select-by-id/solution-approach1.sql
@@ -0,0 +1,14 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+-- Problem     Select By ID
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-09, 08:20 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT *
+FROM CITY
+WHERE ID = 1661 
