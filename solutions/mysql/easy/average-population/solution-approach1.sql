1e53eefd8147ec1fbd62dfd25e31ff343ca2c066
maisha-mehzabin <maishamehzabin0304@gmail.com>
2026-08-09 21:29:15 +0600

    ✅ Average Population [mysql] — approach1

---
+++ solutions/mysql/easy/average-population/solution-approach1.sql
@@ -0,0 +1,13 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/average-population/problem?isFullScreen=true
+-- Problem     Average Population
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-09, 09:29 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT FLOOR(AVG(POPULATION))
+FROM CITY 
