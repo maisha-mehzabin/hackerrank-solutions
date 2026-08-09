9cb8d801fdcf0a7bc5fb06ed2bdceff09f9ee6c3
maisha-mehzabin <maishamehzabin0304@gmail.com>
2026-08-09 21:20:35 +0600

    ✅ Employee Names [mysql] — approach1

---
+++ solutions/mysql/easy/name-of-employees/solution-approach1.sql
@@ -0,0 +1,16 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+-- Problem     Employee Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-09, 09:20 p.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/SELECT NAME 
+FROM EMPLOYEE
+ORDER BY NAME ASC 
