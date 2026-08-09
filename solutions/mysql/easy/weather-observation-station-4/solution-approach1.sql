b7b65d73fae4b097513a30102be9a2a1016daf72
maisha-mehzabin <maishamehzabin0304@gmail.com>
2026-08-09 15:16:05 +0600

    ✅ Weather Observation Station 4 [mysql] — approach1

---
+++ solutions/mysql/easy/weather-observation-station-4/solution-approach1.sql
@@ -0,0 +1,15 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-4/problem?isFullScreen=true
+-- Problem     Weather Observation Station 4
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-09, 03:16 p.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/SELECT COUNT(CITY) - COUNT(DISTINCT CITY) AS difference 
+FROM STATION
